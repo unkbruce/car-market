5df4900e914c5ab2fdf703f6ecaceb843722f8d2
unkbruce <sollcom@naver.com>
2026-06-06 17:52:34 +0000

    차량 샘플 이미지 목록 및 선택 기능 적용

--- client/src/data/sampleCarImages.js
+++ client/src/data/sampleCarImages.js
@@ -1,2 +1,266 @@
-export const SAMPLE_CAR_IMAGES = [];
-
+export const SAMPLE_CAR_IMAGES = [
+  {
+    id: 'audi-a4-2020-white',
+    label: 'Audi A4 2020 White',
+    imageUrls: [
+      '/images/cars/audi-a4-2020-white/audi-a4-2020-white-front-3q.png',
+      '/images/cars/audi-a4-2020-white/audi-a4-2020-white-rear-3q.png',
+      '/images/cars/audi-a4-2020-white/audi-a4-2020-white-side-left.png',
+    ],
+  },
+  {
+    id: 'bmw-x5-2021-white',
+    label: 'Bmw X5 2021 White',
+    imageUrls: [
+      '/images/cars/bmw-x5-2021-white/bmw-x5-2021-white-front-3q.png',
+      '/images/cars/bmw-x5-2021-white/bmw-x5-2021-white-interior-front.png',
+      '/images/cars/bmw-x5-2021-white/bmw-x5-2021-white-interior-rear.png',
+      '/images/cars/bmw-x5-2021-white/bmw-x5-2021-white-rear-3q.png',
+      '/images/cars/bmw-x5-2021-white/bmw-x5-2021-white-rear.png',
+      '/images/cars/bmw-x5-2021-white/bmw-x5-2021-white-side-left.png',
+    ],
+  },
+  {
+    id: 'hyundai-avante-2021-white',
+    label: 'Hyundai Avante 2021 White',
+    imageUrls: [
+      '/images/cars/hyundai-avante-2021-white/hyundai-avante-2021-white-front-3q.png',
+      '/images/cars/hyundai-avante-2021-white/hyundai-avante-2021-white-interior-front.png',
+      '/images/cars/hyundai-avante-2021-white/hyundai-avante-2021-white-interior-rear.png',
+      '/images/cars/hyundai-avante-2021-white/hyundai-avante-2021-white-rear-3q.png',
+      '/images/cars/hyundai-avante-2021-white/hyundai-avante-2021-white-rear.png',
+      '/images/cars/hyundai-avante-2021-white/hyundai-avante-2021-white-side-left.png',
+    ],
+  },
+  {
+    id: 'hyundai-grand-starex-2012-white',
+    label: 'Hyundai Grand Starex 2012 White',
+    imageUrls: [
+      '/images/cars/hyundai-grand-starex-2012-white/hyundai-grand-starex-2012-white-front-3q.png',
+    ],
+  },
+  {
+    id: 'hyundai-grandeur-2026-black',
+    label: 'Hyundai Grandeur 2026 Black',
+    imageUrls: [
+      '/images/cars/hyundai-grandeur-2026-black/hyundai-grandeur-2026-black-front-3q.png',
+      '/images/cars/hyundai-grandeur-2026-black/hyundai-grandeur-2026-black-interior-front.png',
+      '/images/cars/hyundai-grandeur-2026-black/hyundai-grandeur-2026-black-interior-rear.png',
+      '/images/cars/hyundai-grandeur-2026-black/hyundai-grandeur-2026-black-rear-3q.png',
+      '/images/cars/hyundai-grandeur-2026-black/hyundai-grandeur-2026-black-rear.png',
+      '/images/cars/hyundai-grandeur-2026-black/hyundai-grandeur-2026-black-side-left.png',
+    ],
+  },
+  {
+    id: 'hyundai-kona-2021-blue',
+    label: 'Hyundai Kona 2021 Blue',
+    imageUrls: [
+      '/images/cars/hyundai-kona-2021-blue/hyundai-kona-2021-blue-front-3q.png',
+      '/images/cars/hyundai-kona-2021-blue/hyundai-kona-2021-blue-interior-front.png',
+      '/images/cars/hyundai-kona-2021-blue/hyundai-kona-2021-blue-interior-rear.png',
+      '/images/cars/hyundai-kona-2021-blue/hyundai-kona-2021-blue-rear-3q.png',
+      '/images/cars/hyundai-kona-2021-blue/hyundai-kona-2021-blue-rear.png',
+      '/images/cars/hyundai-kona-2021-blue/hyundai-kona-2021-blue-side-left.png',
+    ],
+  },
+  {
+    id: 'hyundai-kona-electric-2021-deep-blue',
+    label: 'Hyundai Kona Electric 2021 Deep Blue',
+    imageUrls: [
+      '/images/cars/hyundai-kona-electric-2021-deep-blue/hyundai-kona-electric-2021-deep-blue-front-3q.png',
+    ],
+  },
+  {
+    id: 'hyundai-maxcruz-2017-silver',
+    label: 'Hyundai Maxcruz 2017 Silver',
+    imageUrls: [
+      '/images/cars/hyundai-maxcruz-2017-silver/hyundai-maxcruz-2017-silver-front-3q.png',
+    ],
+  },
+  {
+    id: 'hyundai-santa-fe-2024-dark-gray',
+    label: 'Hyundai Santa Fe 2024 Dark Gray',
+    imageUrls: [
+      '/images/cars/hyundai-santa-fe-2024-dark-gray/hyundai-santa-fe-2024-dark-gray-front-3q.png',
+      '/images/cars/hyundai-santa-fe-2024-dark-gray/hyundai-santa-fe-2024-dark-gray-interior-front.png',
+      '/images/cars/hyundai-santa-fe-2024-dark-gray/hyundai-santa-fe-2024-dark-gray-interior-rear.png',
+      '/images/cars/hyundai-santa-fe-2024-dark-gray/hyundai-santa-fe-2024-dark-gray-rear-3q.png',
+      '/images/cars/hyundai-santa-fe-2024-dark-gray/hyundai-santa-fe-2024-dark-gray-rear.png',
+      '/images/cars/hyundai-santa-fe-2024-dark-gray/hyundai-santa-fe-2024-dark-gray-side-left.png',
+    ],
+  },
+  {
+    id: 'hyundai-sonata-2016-silver',
+    label: 'Hyundai Sonata 2016 Silver',
+    imageUrls: [
+      '/images/cars/hyundai-sonata-2016-silver/hyundai-sonata-2016-silver-front-3q.png',
+      '/images/cars/hyundai-sonata-2016-silver/hyundai-sonata-2016-silver-interior-front.png',
+      '/images/cars/hyundai-sonata-2016-silver/hyundai-sonata-2016-silver-interior-rear.png',
+      '/images/cars/hyundai-sonata-2016-silver/hyundai-sonata-2016-silver-rear-3q.png',
+      '/images/cars/hyundai-sonata-2016-silver/hyundai-sonata-2016-silver-rear.png',
+      '/images/cars/hyundai-sonata-2016-silver/hyundai-sonata-2016-silver-side-left.png',
+    ],
+  },
+  {
+    id: 'hyundai-starex-2005-silver',
+    label: 'Hyundai Starex 2005 Silver',
+    imageUrls: [
+      '/images/cars/hyundai-starex-2005-silver/hyundai-starex-2005-silver-front-3q.png',
+    ],
+  },
+  {
+    id: 'hyundai-staria-2024-dark-gray',
+    label: 'Hyundai Staria 2024 Dark Gray',
+    imageUrls: [
+      '/images/cars/hyundai-staria-2024-dark-gray/hyundai-staria-2024-dark-gray-front-3q.png',
+    ],
+  },
+  {
+    id: 'hyundai-tucson-ix-2012-white',
+    label: 'Hyundai Tucson Ix 2012 White',
+    imageUrls: [
+      '/images/cars/hyundai-tucson-ix-2012-white/hyundai-tucson-ix-2012-white-front-3q.png',
+      '/images/cars/hyundai-tucson-ix-2012-white/hyundai-tucson-ix-2012-white-interior-front.png',
+      '/images/cars/hyundai-tucson-ix-2012-white/hyundai-tucson-ix-2012-white-interior-rear.png',
+      '/images/cars/hyundai-tucson-ix-2012-white/hyundai-tucson-ix-2012-white-rear-3q.png',
+      '/images/cars/hyundai-tucson-ix-2012-white/hyundai-tucson-ix-2012-white-rear.png',
+      '/images/cars/hyundai-tucson-ix-2012-white/hyundai-tucson-ix-2012-white-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-carnival-2025-black',
+    label: 'Kia Carnival 2025 Black',
+    imageUrls: [
+      '/images/cars/kia-carnival-2025-black/kia-carnival-2025-black-front-3q.png',
+    ],
+  },
+  {
+    id: 'kia-k3-2019-wine-red',
+    label: 'Kia K3 2019 Wine Red',
+    imageUrls: [
+      '/images/cars/kia-k3-2019-wine-red/kia-k3-2019-wine-red-front-3q.png',
+      '/images/cars/kia-k3-2019-wine-red/kia-k3-2019-wine-red-interior-front.png',
+      '/images/cars/kia-k3-2019-wine-red/kia-k3-2019-wine-red-interior-rear.png',
+      '/images/cars/kia-k3-2019-wine-red/kia-k3-2019-wine-red-rear-3q.png',
+      '/images/cars/kia-k3-2019-wine-red/kia-k3-2019-wine-red-rear.png',
+      '/images/cars/kia-k3-2019-wine-red/kia-k3-2019-wine-red-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-k5-2021-white-pearl',
+    label: 'Kia K5 2021 White Pearl',
+    imageUrls: [
+      '/images/cars/kia-k5-2021-white-pearl/kia-k5-2021-white-pearl-front-3q.png',
+      '/images/cars/kia-k5-2021-white-pearl/kia-k5-2021-white-pearl-interior-front.png',
+      '/images/cars/kia-k5-2021-white-pearl/kia-k5-2021-white-pearl-interior-rear.png',
+      '/images/cars/kia-k5-2021-white-pearl/kia-k5-2021-white-pearl-rear-3q.png',
+      '/images/cars/kia-k5-2021-white-pearl/kia-k5-2021-white-pearl-rear.png',
+      '/images/cars/kia-k5-2021-white-pearl/kia-k5-2021-white-pearl-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-k8-2022-black',
+    label: 'Kia K8 2022 Black',
+    imageUrls: [
+      '/images/cars/kia-k8-2022-black/kia-k8-2022-black-front-3q.png',
+      '/images/cars/kia-k8-2022-black/kia-k8-2022-black-interior-front.png',
+      '/images/cars/kia-k8-2022-black/kia-k8-2022-black-interior-rear.png',
+      '/images/cars/kia-k8-2022-black/kia-k8-2022-black-rear-3q.png',
+      '/images/cars/kia-k8-2022-black/kia-k8-2022-black-rear.png',
+      '/images/cars/kia-k8-2022-black/kia-k8-2022-black-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-k9-2022-black',
+    label: 'Kia K9 2022 Black',
+    imageUrls: [
+      '/images/cars/kia-k9-2022-black/kia-k9-2022-front-3q.png',
+    ],
+  },
+  {
+    id: 'kia-morning-2018-ivory',
+    label: 'Kia Morning 2018 Ivory',
+    imageUrls: [
+      '/images/cars/kia-morning-2018-ivory/kia-morning-2018-ivory-front-3q.png',
+      '/images/cars/kia-morning-2018-ivory/kia-morning-2018-ivory-interior-front.png',
+      '/images/cars/kia-morning-2018-ivory/kia-morning-2018-ivory-interior-rear.png',
+      '/images/cars/kia-morning-2018-ivory/kia-morning-2018-ivory-rear-3q.png',
+      '/images/cars/kia-morning-2018-ivory/kia-morning-2018-ivory-rear.png',
+      '/images/cars/kia-morning-2018-ivory/kia-morning-2018-ivory-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-niro-2017-black',
+    label: 'Kia Niro 2017 Black',
+    imageUrls: [
+      '/images/cars/kia-niro-2017-black/kia-niro-2017-black-front-3q.png',
+      '/images/cars/kia-niro-2017-black/kia-niro-2017-black-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-sorento-2022-dark-blue',
+    label: 'Kia Sorento 2022 Dark Blue',
+    imageUrls: [
+      '/images/cars/kia-sorento-2022-dark-blue/kia-sorento-2022-dark-blue-front-3q.png',
+      '/images/cars/kia-sorento-2022-dark-blue/kia-sorento-2022-dark-blue-interior-front.png',
+      '/images/cars/kia-sorento-2022-dark-blue/kia-sorento-2022-dark-blue-interior-rear.png',
+      '/images/cars/kia-sorento-2022-dark-blue/kia-sorento-2022-dark-blue-rear-3q.png',
+      '/images/cars/kia-sorento-2022-dark-blue/kia-sorento-2022-dark-blue-rear.png',
+      '/images/cars/kia-sorento-2022-dark-blue/kia-sorento-2022-dark-blue-side-left.png',
+    ],
+  },
+  {
+    id: 'kia-sorento-2022-white',
+    label: 'Kia Sorento 2022 White',
+    imageUrls: [
+      '/images/cars/kia-sorento-2022-white/kia-sorento-2022-white-front-3q.png',
+    ],
+  },
+  {
+    id: 'lamborghini-urus-2023-yellow',
+    label: 'Lamborghini Urus 2023 Yellow',
+    imageUrls: [
+      '/images/cars/lamborghini-urus-2023-yellow/lamborghini-urus-2023-yellow-front-3q.png',
+      '/images/cars/lamborghini-urus-2023-yellow/lamborghini-urus-2023-yellow-gallery.png',
+      '/images/cars/lamborghini-urus-2023-yellow/lamborghini-urus-2023-yellow-rear-3q.png',
+      '/images/cars/lamborghini-urus-2023-yellow/lamborghini-urus-2023-yellow-rear.png',
+      '/images/cars/lamborghini-urus-2023-yellow/lamborghini-urus-2023-yellow-side-left.png',
+    ],
+  },
+  {
+    id: 'lexus-es-2020-silver',
+    label: 'Lexus Es 2020 Silver',
+    imageUrls: [
+      '/images/cars/lexus-es-2020-silver/lexus-es-2020-silver-front-3q.png',
+      '/images/cars/lexus-es-2020-silver/lexus-es-2020-silver-interior-front.png',
+      '/images/cars/lexus-es-2020-silver/lexus-es-2020-silver-interior-rear.png',
+      '/images/cars/lexus-es-2020-silver/lexus-es-2020-silver-rear-3q.png',
+      '/images/cars/lexus-es-2020-silver/lexus-es-2020-silver-rear.png',
+      '/images/cars/lexus-es-2020-silver/lexus-es-2020-silver-side-left.png',
+    ],
+  },
+  {
+    id: 'mercedes-benz-g-class-2019-white',
+    label: 'Mercedes Benz G Class 2019 White',
+    imageUrls: [
+      '/images/cars/mercedes-benz-g-class-2019-white/mercedes-benz-g-class-2019-white-front-3q.png',
+      '/images/cars/mercedes-benz-g-class-2019-white/mercedes-benz-g-class-2019-white-interior-front.png',
+      '/images/cars/mercedes-benz-g-class-2019-white/mercedes-benz-g-class-2019-white-interior-rear.png',
+      '/images/cars/mercedes-benz-g-class-2019-white/mercedes-benz-g-class-2019-white-rear-3q.png',
+      '/images/cars/mercedes-benz-g-class-2019-white/mercedes-benz-g-class-2019-white-rear.png',
+      '/images/cars/mercedes-benz-g-class-2019-white/mercedes-benz-g-class-2019-white-side-left.png',
+    ],
+  },
+  {
+    id: 'porsche-911-2024-silver',
+    label: 'Porsche 911 2024 Silver',
+    imageUrls: [
+      '/images/cars/porsche-911-2024-silver/porsche-911-2024-silver-front-3q.png',
+      '/images/cars/porsche-911-2024-silver/porsche-911-2024-silver-interior-front.png',
+      '/images/cars/porsche-911-2024-silver/porsche-911-2024-silver-interior-rear.png',
+      '/images/cars/porsche-911-2024-silver/porsche-911-2024-silver-rear-3q.png',
+      '/images/cars/porsche-911-2024-silver/porsche-911-2024-silver-rear.png',
+      '/images/cars/porsche-911-2024-silver/porsche-911-2024-silver-side-left.png',
+    ],
+  },
+];
